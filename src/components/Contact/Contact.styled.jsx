3ec8553d7409e2303c9c83f97c2981dd7eb8a7d3
NezDvora <122118918+NezDvora@users.noreply.github.com>
2023-08-28 00:59:24 +0300

    add

---
+++ src/components/Contact/Contact.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 120px;
+`;
+
+export const Li = styled.li`
+  padding: 10px;
+  display: flex;
+  `;
---
+++ src/components/Form/Form.styled.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const FormTag = styled.form`
+  box-sizing: border-box;
+  width: 360px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Lable = styled.label`
+  display: flex;
+  flex-direction: column;
+`;
+export const Input = styled.input`
+  margin-top: 5px;
+  padding: 5px;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  margin: 10px;
+  margin-bottom: 50px;
+`;
